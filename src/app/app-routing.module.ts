5dee773e73d248b9cd3c2d0913a22a47a863e4af
Carnidev <carniceromartinjuan@gmail.com>
2023-11-10 17:27:39 +0100

    buscar ingrediente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { GenerarComidaComponent } from './components/generar-comida/generar-comida.component';
+import { BuscarIngredienteComponent } from './components/buscar-ingrediente/buscar-ingrediente.component';
 
 const routes: Routes = [
   {path:'dashboard', component:DashboardComponent},
-  {path:'generar-comida', component:GenerarComidaComponent}
+  {path:'generar-comida', component:GenerarComidaComponent},
+  {path:'buscar-ingrediente',component:BuscarIngredienteComponent}
 ];
 
 @NgModule({
