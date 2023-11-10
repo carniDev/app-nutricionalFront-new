f488edb7dfe6b5016548cc20c06b6ca16b4d96ae
Carnidev <carniceromartinjuan@gmail.com>
2023-11-10 17:15:35 +0100

    establecida ruta a generar comida

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { GenerarComidaComponent } from './components/generar-comida/generar-comida.component';
 
 const routes: Routes = [
-  {path:'dashboard', component:DashboardComponent}
+  {path:'dashboard', component:DashboardComponent},
+  {path:'generar-comida', component:GenerarComidaComponent}
 ];
 
 @NgModule({
--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+
+
+constructor(private route:Router){}
+addComida(){
+this.route.navigate(['generar-comida']);
+}
 
 }
