90f5cf587ed03636c912156b12ea58357b136d49
Carnidev <carniceromartinjuan@gmail.com>
2023-11-10 18:08:33 +0100

    creado dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import { EditarComidaComponent } from './components/editar-comida/editar-comida.
 import { BuscarIngredienteComponent } from './components/buscar-ingrediente/buscar-ingrediente.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegistrarUsuarioComponent } from './components/registrar-usuario/registrar-usuario.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ChartsComponent } from './components/charts/charts.component';
+
 
 @NgModule({
   declarations: [
@@ -18,11 +21,15 @@ import { RegistrarUsuarioComponent } from './components/registrar-usuario/regist
     EditarComidaComponent,
     BuscarIngredienteComponent,
     LoginComponent,
-    RegistrarUsuarioComponent
+    RegistrarUsuarioComponent,
+    ChartsComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { Route, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { InformacionNutricional } from 'src/app/models/informacion-nutricional';
+import { DashboardService } from 'src/app/services/dashBoard.services';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,11 +10,17 @@ import { Route, Router } from '@angular/router';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+  informacionNutricional!: Observable<InformacionNutricional>;
+
+  constructor(private route: Router, private dashboardService: DashboardService) {
+    const credentials = { fechaBuscar: '10/11/2023', email: 'juan@email.com' };
+    this.informacionNutricional = this.dashboardService.buscar(credentials);
+  }
 
 
-constructor(private route:Router){}
-addComida(){
-this.route.navigate(['generar-comida']);
-}
+
+  addComida() {
+    this.route.navigate(['generar-comida']);
+  }
 
 }
